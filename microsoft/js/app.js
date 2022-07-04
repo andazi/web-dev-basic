e20b99952c44199d6abb6d498c4ebc259cd73ee7
andazi <andaziboboye@gmail.com>
2022-07-04 21:54:13 +0100

    microsoft web dev for beginners

---
+++ microsoft/js/app.js
@@ -0,0 +1,19 @@
+//  <>  ||  * +  () => {} console.log() :  $  %
+
+
+// fibonacci sequence
+// set of numbers such that each number is the sum of the two preceding ones
+// starting from 0 and 1
+
+// let number = 100;
+// let n1 = 0, n2 = 1, nextTerm;
+// const fib = [];
+
+// for(let i = 1; i <= number; i++){
+//     fib.push(n1);
+//     // console.log(n1);
+//     nextTerm = n1 + n2;
+//     n1 = n2;
+//     n2 = nextTerm;
+// }
+// console.log(fib)
