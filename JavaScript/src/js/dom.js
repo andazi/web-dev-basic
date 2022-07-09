c7797c8df434ffc7e925797e2f56854784f5d94f
andazi <andaziboboye@gmail.com>
2022-07-09 23:42:07 +0100

    poe

--- JavaScript/src/js/dom.js
+++ JavaScript/src/js/dom.js
@@ -3,7 +3,7 @@
 const images = document.querySelector('.images');
 const baseURL = `https://github.com/PokeAPI/sprites/raw/master/sprites/pokemon/other/home/`;
 
-for(let i = 1; i <= 11; i++){
+for(let i = 1; i <= 251; i++){
     const image = document.createElement('img');
     const pokemon = document.createElement('span');
 
