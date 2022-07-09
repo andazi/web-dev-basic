260facdbcc98b101fa6f400f6c1ebeca866e6c1d
andazi <andaziboboye@gmail.com>
2022-07-09 23:41:17 +0100

    pokemon on trial

---
+++ JavaScript/src/js/dom.js
@@ -0,0 +1,13 @@
+// 
+
+const images = document.querySelector('.images');
+const baseURL = `https://github.com/PokeAPI/sprites/raw/master/sprites/pokemon/other/home/`;
+
+for(let i = 1; i <= 11; i++){
+    const image = document.createElement('img');
+    const pokemon = document.createElement('span');
+
+    image.src =  `${baseURL}${i}.png`
+    images.appendChild(image)
+}
+
---
+++ profile card/card1/src/app.js
@@ -0,0 +1,4 @@
+const profile = document.querySelectorAll('.profile img');
+for(let attribute of profile){
+    attribute.setAttribute('alt', 'profile image')
+}
