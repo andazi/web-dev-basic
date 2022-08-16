14bdd33b288c4fdca9e49c917dc98d8a8ec0fe54
Boboye <andaziboboye@gmail.com>
2022-08-16 23:27:54 +0100

    created all necessary classes

--- tv_show/src/app.js
+++ tv_show/src/app.js
@@ -1,5 +1,38 @@
 const toggle = document.querySelector('.toggle-btn');
+const topTvShow = document.querySelector('main');
+
+// dark-light mode toggle 
 
 toggle.addEventListener('click', () =>{
     toggle.classList.toggle('change');
 })
+
+
+let topSection = document.createElement('section');
+topTvShow.
+topTvShow.classList.add('top-tv');
+topTvShow.innerHTML = `
+<div class="top-tv-heading">
+<div class="top-title>
+<p class="title">
+${title}
+</p>
+</div>
+<div class="summary">
+<p class"detail">
+${detail}
+</p>
+</div>
+</div>
+`
+
+
+
+
+
+
+
+
+
+
+
