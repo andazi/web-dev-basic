05af564d8696cfd5d9ff24e751ca89aa682d315f
Boboye <andaziboboye@gmail.com>
2022-11-13 17:31:34 +0100

    question card

--- codeWars/roman.js
+++ codeWars/roman.js
@@ -1,38 +1,50 @@
-// not done
-function numeral_to_roman(num) {
-    let romanNumeral = {
-        1000: 'M',
-        500: 'D',
-        100: 'C',
-        50: 'L',
-        10: 'X',
-        5: 'V',
-        1: 'I'
-    };
+// // nt working
+// function num_to_roman(num) {
+//     let romanNumeral = {
+//         1000: "M",
+//         500: " D",
+//         100: "C",
+//         50: "L",
+//         10: "X",
+//         5: " V",
+//         1: "I"
+//     };
 
-    // my logic should start from the highest
-    // find 23; 23 - 1000, 23 -500, 23 -100, 23 -50, 23 -10 => 2 * 3
-    // 3 -1000 .......
+// let result = "";
+// while (num >= 0) {
+//     for (let numeral in romanNumeral) {
+//         let roman = romanNumeral[numeral];
 
-    let result = "";
-    while (num >= 0) {
-        for (const numerals in romanNumeral) {
-            let roman = romanNumeral[numerals];
-            // console.log(numerals);
-            if (num - numerals > 0) {
-                result += roman;
-                // console.log(result)
-                // num = 0;
-            } else if (num - numerals === 0) {
-                result += roman;
-            } else {
-                result += roman;
-                num -= numerals;
-            }
-        }
-        return result
-    }
+//         if (num - numeral > 0) {
+//             // let roman = romanNumeral[numeral];
+//             result += roman;
+//             num -= numeral;
+//             console.log(num)
+//         }
+//     }
+//     // return result
+//     console.log(result)
 
+//     }
+// }
+// num_to_roman(23);
+
+let roman = {
+    1: "I",
+    5: " V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M"
+};
+
+const romanNumeral = [];
+
+for (let x in roman) {
+    let num = roman[x];
+    roman.push(`
+    ${x}: "${num}"
+    `)
+    console.log(romanNumeral)
 }
-
-numeral_to_roman(23);
